0d65a93273646d8267db381e505489675ca511df
Steven Colburn and Swati Sahay <steven@>
2018-05-31 13:33:12 -0700

    add contact toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
-function makeContact(userName,uniqueID)
+function styleList(input)
 {
   var tempString = "";
-  return tempString.concat("<li>",userName,'<ul class="test-hide"><li>Dummy Text</li></ul>',"</li>");
+  return tempString.concat("<li>",input,"</li>");
+}
+
+function makeContact(contactName,input)
+{
+  var tempString = "";
+  return tempString.concat("<li>",contactName,'<ul class="initial-hide">',input,'</ul>',"</li>");
 
 }
 
@@ -9,28 +15,38 @@ function makeContact(userName,uniqueID)
 $(function() {
   $("#btn-submit").click(function()
   {
-    var userName = "";
+    var contactName = "";
     var address1 = "";
     var address2 = "";
-    var address3 = ";"
     var city = "";
     var state = ""
     var zipcode = "";
+    var contactEmail="";
+    var contactNumber="";
 
-    userName = $("#fullName").val();
+    contactName = $("#fullName").val();
     address1 = $("#address1").val();
     address2 = $("#address2").val();
     city = $("#city").val();
     state = $("#state").val();
     zipcode = $("#zipcode").val();
+    contactEmail = $("#contactEmail").val();
+    contactNumber= $("#contactNumber").val();
 
-    address3 = city.concat(", ",state," ",zipcode);
+    var address3 = city.concat(", ",state," ",zipcode);
 
-    $("#contact-list").prepend(makeContact(userName));
+    address1 = styleList(address1);
+    address2 = styleList(address2);
+    address3 = styleList(address3);
+    contactEmail = styleList(contactEmail);
+    contactNumber = styleList(contactNumber);
+    var input = address1.concat(address2,address3,contactEmail,contactNumber);
+
+    $("#contact-list").prepend(makeContact(contactName,input));
 
     $("ul#contact-list").children("li").first().click(function()
     {
-      $(this).find("ul.test-hide").toggle();
+      $(this).find("ul.initial-hide").toggle();
     });
 
     event.preventDefault();
